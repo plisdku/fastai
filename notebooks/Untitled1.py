8bcb898d8ce8452c90c1a39975f39868ad2b1ef2
Paul Hansen <paul.c.hansen@gmail.com>
2025-05-24 19:01:36 -0700

    blah

---
+++ notebooks/Untitled1.py
@@ -0,0 +1,16 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.1
+#   kernelspec:
+#     display_name: fastai
+#     language: python
+#     name: fastai
+# ---
+
+# %%

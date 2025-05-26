16f805f04101508f4b1a88e65bf234d14f55f03c
Paul Hansen <paul.c.hansen@gmail.com>
2025-05-25 18:11:55 -0700

    add check for pytorch supporting MPS devices

---
+++ notebooks/pytorch test.py
@@ -0,0 +1,32 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.1
+#   kernelspec:
+#     display_name: fastai
+#     language: python
+#     name: fastai
+# ---
+
+# %%
+# %load_ext autoreload
+# %autoreload 2
+# %xmode plain
+
+# %%
+import torch
+
+print(f"MPS available: {torch.backends.mps.is_available()}")
+print(f"MPS built: {torch.backends.mps.is_built()}")
+
+if torch.backends.mps.is_available():
+    mps_device = torch.device("mps")
+    x = torch.ones(1, device=mps_device)
+    print(x)
+else:
+    print("MPS device not found.")
